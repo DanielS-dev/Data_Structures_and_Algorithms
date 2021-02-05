1f90c02eb81791844f5db061e7142fba51809976
Daniel Silva <daniel_santos2002@outlook.com>
2021-02-05 07:53:47 -0200

    First Course

---
+++ Algorithmic Toolbox/week1_programming_challenges/1_sum_of_two_digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int numberOne;
+	int numberTwo;
+	int sum;
+	
+	scanf ("%d", &numberOne);
+	scanf ("%d", &numberTwo);
+	
+	sum = numberOne + numberTwo;
+	
+	printf ("%d",sum);
+	
+	return 0;
+};
